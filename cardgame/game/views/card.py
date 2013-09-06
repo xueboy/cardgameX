ee356cd7b68e7d10feece23c6ac809ebed291f73
Danile <xueboy163@163.com>
2013-09-06 10:28:20 +0800

    new version

---
+++ cardgame/game/views/card.py
@@ -0,0 +1,26 @@
+﻿#coding:utf-8
+#!/usr/bin/env python
+
+from django.http import HttpResponse
+from gclib.gcjson import gcjson
+
+
+def set_team(request):
+ 	 usr = request.user
+ 	 inv = usr.getInventory()
+ 	 cardid1 = request.GET['card_id1']
+ 	 cardid2 = request.GET['card_id2']
+ 	 cardid3 = request.GET['card_id3']
+ 	 cardid4 = request.GET['card_id4']
+ 	 team = inv.setTeam(cardid1, cardid2, cardid3, cardid4)
+ 	 inv.save()
+ 	 return HttpResponse(gcjson.dumps(team))
+
+
+def set_leader(request):
+	usr = request.user
+	inv = usr.getInventory()
+	cardid = request.GET['card_id']	
+	team = inv.setLeader(cardid)
+	inv.save()
+	return HttpResponse(gcjson.dumps(team))
---
+++ cardgame/game/views/friend.py
@@ -0,0 +1,24 @@
+﻿#coding:utf-8
+#!/usr/bin/env python
+
+from django.http import HttpResponse
+from gclib.gcjson import gcjson
+
+def friend_request(request):
+	usr = request.user
+	friendid = request.GET['friend_id']
+	friend = user.get(friendid)
+	if friend != None:		
+		friend.addFriendRequest(usr.roleid, {'name': usr.name, 'level': usr.level, 'last_login': usr.last_login, 'leader': usr.leader})
+	return HttpResponse(gcjson.dumps('OK'))
+		
+		
+def friend_confirm(request):
+	usr = request.user
+	isConfirm = request.GET['is_confirm']
+	friendid = request.GET['friend_id']
+	friend = user.get(friendid)
+	if friend != None:		
+		usr.confirmFriendRequest(friend, isConfirm)		
+	return HttpResponse(gcjson.dumps({'friends': usr.friends, 'friend_request': usr.friend_request}))
+		
