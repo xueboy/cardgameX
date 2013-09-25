b3671b567b82052c35adbe9ad759e2741740437b
Daniel <xueboy163@163.com>
2013-09-25 19:09:57 +0800

    new version.

--- cardgame/game/views/dungeon.py
+++ cardgame/game/views/dungeon.py
@@ -27,30 +27,29 @@ def enter(request):
 	dunConf = config.getConfig('dungeon')
 	
 	if dun.canEnterNormal(dunConf, battleid, fieldid) == True:
-		reinforce = dun.getReinforcement()
+	#	reinforce = dun.getReinforcement()
 		dun.setCurrentField(battleid, fieldid)
 		dun.save()
-		return HttpResponse(gcjson.dumps({'reinforce':reinforce}))
-	return HttpResponse('field_not_available')
+	#	return HttpResponse(gcjson.dumps({'reinforce':reinforce}))
+	#return HttpResponse('field_not_available')
 	
 	
-def start(request):
-	print (time.clock() * 10000)
-	reinforceid = request.GET['reinforce_id']	
+#def start(request):	
+	#reinforceid = request.GET['reinforce_id']	
 	usr = request.user
 	dun = usr.getDungeon()
 	conf = config.getConfig('pet')
 	dunConf = config.getConfig('dungeon')	
 	leader = None
 	reinforce = None
-	if dun.curren_field['battleid'] == '' or dun.curren_field['fieldid'] == '':
-		return HttpResponse(gcjson.dumps({'msg':'field_not_enter'}))		
-	if dun.isReinforceExist(reinforceid):
-		reinforce = user.get(reinforceid)
-		rei_inv = reinforce.getInventory()
-		leaderid = rei_inv.team[0]	
-		leader = rei_inv.getCard(leaderid)
-		dun.reinforced_list.append(reinforceid)	
+	#if dun.curren_field['battleid'] == '' or dun.curren_field['fieldid'] == '':
+	#	return HttpResponse(gcjson.dumps({'msg':'field_not_enter'}))		
+	#if dun.isReinforceExist(reinforceid):
+	#	reinforce = user.get(reinforceid)
+	#	rei_inv = reinforce.getInventory()
+	#	leaderid = rei_inv.team[0]	
+	#	leader = rei_inv.getCard(leaderid)
+	#	dun.reinforced_list.append(reinforceid)	
 	
 	for battleConf in dunConf:
 		if battleConf['battleId'] == dun.curren_field['battleid']:
@@ -61,17 +60,17 @@ def start(request):
 					if usr.stamina < staminaCost:
 						return HttpResponse(gcjson.dumps({'msg':'not_enught_stamina'}))
 					usr.stamina = usr.stamina - staminaCost												
-					goldCast = 0
-					if leader != None:
-						start = conf[leader['id']]['star']
-						level = leader['leader']
-						goldCost = reinforce_price[star - 1][0] + reinforce_price[star - 1][1] * level
-						if usr.gold < goldCost:
-							return HttpResponse(gcjson.dumps({'msg':'not_enught_gold'}))
-						usr.gold = usr.gold - goldCost
-						reinforce.gold = reinforce.gold + goldCast
-						reinforce.save()
-						usr.save()												
+		#			goldCast = 0
+		#			if leader != None:
+		#				start = conf[leader['id']]['star']
+		#				level = leader['leader']
+		#				goldCost = reinforce_price[star - 1][0] + reinforce_price[star - 1][1] * level
+		#				if usr.gold < goldCost:
+		#					return HttpResponse(gcjson.dumps({'msg':'not_enught_gold'}))
+		#				usr.gold = usr.gold - goldCost
+		#				reinforce.gold = reinforce.gold + goldCast
+		#				reinforce.save()
+					usr.save()												
 					data = {}
 					data['wave_arrages'] = waves
 					data['gold'] = usr.gold
