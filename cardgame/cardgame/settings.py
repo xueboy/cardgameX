842e071122332159af6d6e26be324acaebca6afe
Daniel <xueboy163@163.com>
2013-10-11 18:47:45 +0800

    new version.

--- cardgame/cardgame/settings.py
+++ cardgame/cardgame/settings.py
@@ -71,8 +71,8 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT =r'D:/Project/cardgameX/cardgame/game/static'
-
+#STATIC_ROOT =r'D:/Project/cardgameX/cardgame/game/static'
+STATIC_ROOT =r'\\192.168.0.7\static'
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_URL = '/static/'
