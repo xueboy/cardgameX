932a6049a0954b7ffe12888928d0214e7e1e14ec
Daniel <xueboy163@163.com>
2014-02-10 10:49:08 +0800

    new version.

--- cardgame/game/routine/signin.py
+++ cardgame/game/routine/signin.py
@@ -67,7 +67,7 @@ class signin:
 		
 		awd = {}
 		awd = drop.open(usr, signinAward['dropid'], awd)
-		awd = drop.makeData(awd)
+		awd = drop.makeData(awd, {})
 		usr.signin['last_signin_time'] = now
 		usr.save()
 		return awd		
