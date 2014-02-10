1aea0dde57e6d59def2cad39bf5dc9124c9add96
Daniel <xueboy163@163.com>
2014-02-10 18:02:58 +0800

    new version.

---
+++ cardgame/game/views/mall.py
@@ -0,0 +1,39 @@
+﻿#coding:utf-8
+#!/usr/bin/env python
+
+
+def purchase(request):
+	
+	purchaseid = request.GET['purchase_id']
+	
+	usr = request.user	
+	mallPriceConf = config.getConfig('mall_price')
+	
+	if not mallPriceConf.has_key(purchaseid):
+		return {'msg':'puchaseid_not_exist'}
+			
+	mallPriceInfo = mallPriceConf[puchaseid]	
+	data = {}
+	if mallPriceInfo['pricePromote'] > 0:
+		costGold = mallPriceInfo['pricePromote']
+		if costGold > usr.gold:
+			return {'msg':'gold_not_enough'}		
+		usr.gold = usr.gold - costGold
+		data['gold'] = usr.gold
+	if mallPriceInfo['gemPromote'] >0:
+		costGem = mallPriceInfo['gemPromote']
+		if costGem > usr.gem:
+			return {'msg':'gem_not_enough'}
+		usr.gem = usr.gem - costGem
+		data['gem'] = usr.gem
+		
+	inv = usr.getInventory()
+	upIt, newIt = inv.addItem(mallPriceInfo['itemid'])
+	
+	if upIt:
+		data['update_item'] = upIt
+	
+	if newIt:
+		data['add_item'] = newIt
+		
+	return data
