341dac2d3eda21bc37f983ceff3604b4f6fe0ef0
Daniel <xueboy163@163.com>
2014-02-24 11:44:56 +0800

    new version.

---
+++ cardgame/game/routine/gift.py
@@ -0,0 +1,11 @@
+﻿#coding:utf-8
+#!/usr/bin/env python
+
+from gclib.curl import curl
+from gclib.json import json
+
+class gift:
+	
+	@staticmethod
+	def sendGift(sendRoleid, receiveRoleid):
+		return json.loads(curl.url(ARENE_SERVER +  '/arena/network_gift/', None, {'send_roleid':sendRoleid, 'receive_roleid': receiveRoleid}))
