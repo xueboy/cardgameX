981d384a80e3935958f8c7a2a436dc1e46aa8da5
Daniel <xueboy163@163.com>
2014-03-12 19:55:11 +0800

    new version.

--- cardgame/arenarank/models/tower_ladder.py
+++ cardgame/arenarank/models/tower_ladder.py
@@ -185,8 +185,11 @@ class tower_ladder(facility):
 		"""
 		显示位置
 		"""
-		roleid = rank[position]['roleid']				
-		return {'roleid':roleid, 'name':item[roleid]['name'], 'level':item[roleid]['level'], 'position':position, 'in_ladder_day_count': item[roleid]['in_ladder_day_count'], 'point': item[roleid]['point'], 'floor': item[roleid]['floor']}
+		try:
+			roleid = rank[position]['roleid']				
+			return {'roleid':roleid, 'name':item[roleid]['name'], 'level':item[roleid]['level'], 'position':position, 'in_ladder_day_count': item[roleid]['in_ladder_day_count'], 'point': item[roleid]['point'], 'floor': item[roleid]['floor']}
+		except:
+			return {}
 		
 
 		
